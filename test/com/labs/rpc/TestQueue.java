dd1cf697d790bc3acd6c82b24caa15dbe36e4a7d
Benjamin Dezile <benjdezi@aol.com>
2011-10-14 01:51:22 -0700

    Unit test for Queue

---
+++ test/com/labs/rpc/TestQueue.java
@@ -0,0 +1,48 @@
+package com.labs.rpc;
+
+import junit.framework.*;
+import org.junit.Test;
+
+import com.labs.rpc.util.Queue;
+
+public class TestQueue extends TestCase {
+	
+	@Test
+	public void testPut() {
+		Queue<Object> q = new Queue<Object>();
+		assertEquals(q.size(),0);
+		q.put(new Object());
+		assertEquals(q.size(),1);
+	}
+	
+	@Test
+	public void testGet() {
+		Object o = new Object();
+		Queue<Object> q = new Queue<Object>(); 
+		q.put(o);
+		assertEquals(q.get(),o);
+		assertEquals(q.size(),0);
+	}
+	
+	@Test
+	public void testGetWithTimeout() {
+		int timeout = 1;
+		Object o = new Object();
+		Queue<Object> q = new Queue<Object>(); 
+		long t = System.currentTimeMillis();
+		try {
+			assertNull(q.get(timeout));
+			long dt = System.currentTimeMillis() - t;
+			assertTrue("Did not wait the given amount of time ("+dt+","+(timeout*1000)+")", dt >= timeout*1000);
+		} catch (InterruptedException e) {
+			fail("There is no reason to be interrupted here");
+		}
+		try {
+			q.put(o);
+			assertEquals(q.get(timeout),o);
+		} catch (Exception e) {
+			fail("Should not have failed here");
+		}
+	}
+		
+}
