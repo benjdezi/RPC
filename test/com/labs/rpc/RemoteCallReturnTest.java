49d74fc9d52847ca2f50a90a62d6154af0aa74ba
Benjamin Dezile <benjdezi@aol.com>
2011-10-21 02:47:30 -0700

    Added more conditions to test

--- test/com/labs/rpc/RemoteCallReturnTest.java
+++ test/com/labs/rpc/RemoteCallReturnTest.java
@@ -17,13 +17,16 @@ public class RemoteCallReturnTest extends TestCase {
 	@Test
 	public void testFromBytes() {
 		RemoteCall rc = new RemoteCall("methodName");
+		assertEquals(rc.seq, 1);
 		Object[] vals = new Object[]{2, 4.5, true, null, new Object[]{"1",1,false}, new JSONObject(), new JSONArray(), new ArrayList<String>(0), new RemoteException("test")};
 		for (Object val:vals) {
 			try {
 				RemoteCallReturn rcr1 = new RemoteCallReturn(rc, val);
+				assertEquals(rcr1.seq, rc.seq);
 				DataPacket dp = RemoteCall.fromBytes(rcr1.getBytes());
 				RemoteCallReturn rcr2 = RemoteCallReturn.fromPacket(dp);
 				assertNotNull(rcr2);
+				assertEquals(rcr2.seq, rc.seq);
 				assertTrue(rcr1.equals(rcr2));
 			} catch (Exception e) {
 				fail("There should not have been any exception: " + e.getMessage());
